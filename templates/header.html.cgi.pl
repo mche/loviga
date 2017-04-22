e0ddfccd7ffe4fe5ae1aeaec486e340c270ae9cb
Mikhail Che <ernesto.chege@yandex.ru>
2017-04-22 08:10:20 +0500

    иконки

--- templates/header.html.cgi.pl
+++ templates/header.html.cgi.pl
@@ -44,7 +44,8 @@ div({-class=>"nav-wrapper",},
     : $c->match->endpoint && $c->match->endpoint->name ne 'profile'
       ? li({},
           a({-class=>"btn-large000 black-text000 teal000 lighten-1000", -href=>$c->url_for('profile')->query(from=>$c->url_for->path), -title=>"Вход/Регистрация",},#btn-large000 black-text000 teal000 lighten-1000
-            i({-class=>"material-icons",}, 'exit_to_app'),
+            #~ i({-class=>"material-icons",}, 'exit_to_app'),
+            i({-class=>"icon-login-1 blue-text text-darken-3"}, ''),
             span({-class=>"hide-on-small-only",}, 'Вход/Регистрация',),
             
           ),
