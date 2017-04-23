b0627c5ac925fbcbfa957610492b82b8bf02bcd0
Mikhail Che <ernesto.chege@yandex.ru>
2017-04-23 21:28:49 +0500

    иконки

--- static/android/js/controllers.js
+++ static/android/js/controllers.js
@@ -623,7 +623,7 @@ angular.module('main')
     var $ctrl = this;
     $scope.User = User;
     
-    $ctrl.$onInit000 = function() {
+    $ctrl.$onInit = function() {
       $timeout(function() {
         //~ console.log("menuNav"+$($element[0]).closest('ons-splitter').get(0).ng339);
         //~ console.log("menuNav"+);
@@ -634,7 +634,7 @@ angular.module('main')
       });
     };
     
-    $ctrl.Init = function(splitter) {
+    $ctrl.Init00 = function(splitter) {
       $timeout(function() {
         console.log("menuNav"+splitter);
         $ctrl.ready = true;
