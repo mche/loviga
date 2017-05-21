73a9b77213574325139f1cea77417f6489ef647a
Mikhail Che <ernesto.chege@yandex.ru>
2017-05-22 01:13:53 +0500

    первая страница

--- templates/footer.html.cgi.pl
+++ templates/footer.html.cgi.pl
@@ -2,7 +2,7 @@ footer({-class=>"page-footer teal darken-2"},
 div({-class=>"container",}, # $c->content('footer000') ||
   div({-class=>"row",},
 
-    div({-class=>"col l8 m8 s12",},
+    div({-class=>"col l7 m7 s12",},
       h4({-class=>"white-text",},"Дружите с нами в социальных сетях"),
       ul({-class=>"",},
         li({-class=>"inline",}, a({-href=>"https://vk.com/lovigazel", -target=>"_blank", -class=>"btn000 white-text waves-effect waves-light",}, img({-src=>"/i/logo/vkontakte.png", -class=>"circle white", -style=>"height: 40px; vertical-align: middle;", -alt=>"vkontakte logo"}), span({-class=>"",}, "ontakte")),),
@@ -11,8 +11,8 @@ div({-class=>"container",}, # $c->content('footer000') ||
         
       ),
     ),
-    div({-class=>"col l4 s12",},
-      a({-class=>"btn-large black", -href=>"https://play.google.com/store/apps/details?id=ru.lovigazel",  -target=>"_blank",  -title=>"Загрузить андроид-приложение",},
+    div({-class=>"col l5 m5 s12",},
+      a({-class=>"btn black", -href=>"https://play.google.com/store/apps/details?id=ru.lovigazel",  -target=>"_blank",  -title=>"Загрузить андроид-приложение",},
         img({-src=>"/i/logo/google-play.png", -class=>"white000", -style=>"height: 3rem;vertical-align: middle;", -alt=>"android google play"}, ''),
         span({}, 'Загрузить с Google Play',),
         
