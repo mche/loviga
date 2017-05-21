5f84f6b9ab736293eca942ee4d4d97d126fcaeef
Mikhail Che <ernesto.chege@yandex.ru>
2017-05-21 23:21:25 +0500

    первая страница

--- templates/footer.html.cgi.pl
+++ templates/footer.html.cgi.pl
@@ -1,25 +1,25 @@
 '<footer class="page-footer">',
 div({-class=>"container",}, $c->content('footer')
  ||
-div({-class=>"row",
-  #~ div({-class=>"col l4 s12",},
-            #~ <h5 class="white-text">J проекту</h5>
-            #~ <p class="grey-text text-lighten-4"></p>
+  div({-class=>"row",
+    #~ div({-class=>"col l4 s12",},
+              #~ <h5 class="white-text">J проекту</h5>
+              #~ <p class="grey-text text-lighten-4"></p>
 
-  #~ ),
-  div({-class=>"col s12",},
-    h4({-class=>"white-text",},"Дружите с нами в социальных сетях"),
-    ul({},
-      li({}, a({-href=>"https://vk.com/lovigazel", -target=>"_blank", -class=>"btn waves-effect waves-light",}, img({-src=>"/i/logo/vkontakte.png", -class=>"circle white", -style=>"height: 40px; vertical-align: middle;", -alt=>"vkontakte logo"}), span({-class=>"",}, "ontakte")),),
-      
-      
-      
+    #~ ),
+    div({-class=>"col s12",},
+      h4({-class=>"white-text",},"Дружите с нами в социальных сетях"),
+      ul({},
+        li({}, a({-href=>"https://vk.com/lovigazel", -target=>"_blank", -class=>"btn waves-effect waves-light",}, img({-src=>"/i/logo/vkontakte.png", -class=>"circle white", -style=>"height: 40px; vertical-align: middle;", -alt=>"vkontakte logo"}), span({-class=>"",}, "ontakte")),),
+        
+        
+        
+      ),
     ),
   ),
+  div({-class=>"footer-copyright",},
+    div({-class=>"",}, "© 2017 LoviGazel Inc. Права защищены."),
+  ),
 ),
-div({-class=>"footer-copyright",},
-  div({-class=>"",}, "© 2017 LoviGazel Inc. Права защищены."),
-),
-,),
 
 '</footer>',
