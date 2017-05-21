8cec247923cc37877cd3be1c21c6a0f7e72b929c
Mikhail Che <ernesto.chege@yandex.ru>
2017-05-22 00:39:07 +0500

    первая страница

--- templates/main/home-nonauth.html.cgi.pl
+++ templates/main/home-nonauth.html.cgi.pl
@@ -1,12 +1,19 @@
 
 
-div({-class=>"right",},
-  a({-class=>"btn-large", -href=>$c->url_for('profile')->query(from=>$c->url_for->path), -title=>"Вход/Регистрация",},
+ul({-class=>"right",},
+  li({}, a({-class=>"btn-large", -href=>$c->url_for('profile')->query(from=>$c->url_for->path), -title=>"Вход/Регистрация",},
     #~ i({-class=>"material-icons",}, 'exit_to_app'),
     i({-class=>"icon-login-1", -style=>"display: inline; vertical-align: middle;"}, ''),
     span({}, 'Вход / Регистрация',),
     
-  ),
+  ),),
+  
+  li({}, a({-class=>"btn-large", -href=>"https://play.google.com/store/apps/details?id=ru.lovigazel", -title=>"Загрузить андроид-приложение с Google Play",},
+    #~ i({-class=>"material-icons",}, 'exit_to_app'),
+    img({-src="/i/logo/google-play.png", -class=>"", -style=>"vertical-align: middle;"}, ''),
+    span({}, 'Загрузить с Google Play',),
+    
+  ),),
 ),
 
 h1("Добро пожаловать на сервис грузоперевозок “Лови Газель”!"),
