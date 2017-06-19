de51232c30f6d3176459a8f444598774b77d04e3
Mikhail Che <ernesto.chege@yandex.ru>
2017-06-20 01:01:09 +0500

    категории

--- templates/main/top-category-list.html.cgi.pl
+++ templates/main/top-category-list.html.cgi.pl
@@ -2,7 +2,7 @@
 
 ul({},
   map {
-    li({-class=>"inline chip",}, a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}),}, "$_->{title} ($_->{count})"),);
+    li({-class=>"inline chip", -style="margin: 0.3rem 0rem;",}, a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}),}, "$_->{title} ($_->{count})"),);
     
   } @{$c->stash('топ-категории')},
 
