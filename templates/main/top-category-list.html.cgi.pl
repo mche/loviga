bfefe430bfa478712cac5cb21bba329b10af8c9b
Mikhail Che <ernesto.chege@yandex.ru>
2017-06-25 16:46:59 +0500

    рубрикатор

--- templates/main/top-category-list.html.cgi.pl
+++ templates/main/top-category-list.html.cgi.pl
@@ -1,8 +1,8 @@
 
 
-($c->stash('топ-категории') || '') && ul({-class=>"clearfix",},
+($c->stash('топ-категории') || '') && ul({-class=>"clearfix center", -style=>"padding:1rem;"},
   map {
-    li({-class=>"inline chip", -style=>"margin: 0.1rem 0rem;",}, a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}),}, "$_->{title} ($_->{count})"),);
+    li({-class=>"inline", -style=>"margin: 0.1rem 0rem;",}, a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}),}, "$_->{title} ($_->{count})"),);
     
   } @{$c->stash('топ-категории')},
 
