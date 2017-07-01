4d1ecf3408edb69ed752d7476716b83b18156a11
Mikhail Che <ernesto.chege@yandex.ru>
2017-07-01 09:56:15 +0500

    рубрикатор

--- templates/main/top-category-list.html.cgi.pl
+++ templates/main/top-category-list.html.cgi.pl
@@ -1,9 +1,19 @@
+return
+  unless $c->stash('топ-категории');
 
+my $rows = @{$c->stash('топ-категории')}/3 + (@{$c->stash('топ-категории')}%3 ? 1 : 0);
 
-($c->stash('топ-категории') || '') && ul({-class=>"clearfix center", -style=>"padding:1rem;"},
+table({-class=>"clearfix", -style=>"padding:1rem;"},
+tbody({},
   map {
-    li({-class=>"inline", -style=>"padding: 0rem 0.5rem;",}, a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}), -class=>"hover",}, "$_->{title} ($_->{count})"),);
+    Tr({-class=>"", -style000=>"padding: 0rem 0.5rem;",},
+      map {
+        td(
+          a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}), -class=>"hover",}, "$_->{title} ($_->{count})"),
+        );
+      } @{$c->stash('топ-категории')}[($_*3)..($_*3+2)],
+    );
     
-  } @{$c->stash('топ-категории')},
+  } (0..($rows-1)),
 
-),
+),),
