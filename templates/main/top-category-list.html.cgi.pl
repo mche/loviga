5ad0b6d34ca6ae94d9cec70dbb3b555cd68b6fa1
Your Name <ernesto.chege@yandex.ru>
2017-09-04 13:30:55 +0500

    top-nav

--- templates/main/top-category-list.html.cgi.pl
+++ templates/main/top-category-list.html.cgi.pl
@@ -3,13 +3,16 @@ return
 
 my $rows = @{$c->stash('топ-категории')}/3 + (@{$c->stash('топ-категории')}%3 ? 1 : 0);
 
-table({-class=>"clearfix", -style=>""},
+table({-class=>"container clearfix", -style=>""},
 tbody({},
   map {
     Tr({-class=>"", },
       map {
         td({-style=>"padding: 0rem 1rem; width:33.3%",},
-          a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}), -class=>"hover",}, "$_->{title} ($_->{count})"),
+          a({-href=>$c->url_for('поиск транспорта')->query(c=>$_->{id}), -class=>"hover fs14 black-text nowrap",},
+            span($_->{title}),
+            sup({-class=>"chip fs8 black-text"}, $_->{count}),
+          ),
         );
       } @{$c->stash('топ-категории')}[($_*3)..($_*3+2)],
     );
